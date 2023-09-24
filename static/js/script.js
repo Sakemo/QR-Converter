3451f5e7d0cba0b536b688221874327b84f764f5
King <39223901+Sakemo@users.noreply.github.com>
2023-09-24 19:35:27 -0300

    Update script.js
    Otimização do código de geração de QR Codes

--- static/js/script.js
+++ static/js/script.js
@@ -2,31 +2,26 @@ const QR_BOX = document.getElementById('qrCont');
 const QR_IMG = document.getElementById('QR_IMG');
 const QR_DATA = document.getElementById('qr_data');
 
-function Generate(){
-    if(QR_DATA.value.length > 0){
-        var regex = /^[0-9]+$/;
-        if (regex.test(QR_DATA.value)) {
-            var qrData = QR_DATA.value;
-            if (qrData.startsWith('+')){
-                QR_IMG.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&
-                data=` + 'https://wa.me/' + QR_DATA.value;
-                QR_BOX.classList.add("show");        
-            }
-            else{
-                QR_IMG.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&
-                data=` + 'https://wa.me/' + "+55" +QR_DATA.value;
-                QR_BOX.classList.add("show");           
-            }
+function generateQRCode(data) {
+    return `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(data)}`;
+}
+
+function Generate() {
+    const inputValue = QR_DATA.value.trim();
+    
+    if (inputValue.length > 0) {
+        let qrData = inputValue;
+
+        if (!/^\d+$/.test(qrData)) {
+            qrData = 'https://wa.me/' + (qrData.startsWith('+') ? qrData : '+55' + qrData);
         }
-        else{
-            QR_IMG.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&
-            data=` + QR_DATA.value;
-            QR_BOX.classList.add("show");
-        }
-    }else{
-        QR_DATA.classList.add('error')
-        setTimeout(()=>{
-            QR_DATA.classList.remove('error')
+
+        QR_IMG.src = generateQRCode(qrData);
+        QR_BOX.classList.add('show');
+    } else {
+        QR_DATA.classList.add('error');
+        setTimeout(() => {
+            QR_DATA.classList.remove('error');
         }, 500);
-    };
-};
+    }
+}
